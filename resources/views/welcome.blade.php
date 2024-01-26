3fff22c3e5195e1766115df4dd845a11bae34795
OgataiKhan <badellino.kevin@gmail.com>
2024-01-26 17:51:14 +0100

    feat: basic layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Comics</title>
-    <!-- Google Font Asap & Asap Condensed -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Asap+Condensed:wght@400;500&family=Asap:wght@400;500&display=swap"
-        rel="stylesheet">
-    <!-- /Google Font Asap & Asap Condensed -->
-    {{-- JS & CSS --}}
-    @vite('resources/js/app.js')
-    {{-- /JS & CSS --}}
-</head>
-
-<body>
-    <h1>Ciao</h1>
-</body>
-
-</html>
+@section('main')
+    <main>
+        <h1>Ciao</h1>
+    </main>
+@endsection
